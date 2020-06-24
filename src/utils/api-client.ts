0cc378caa8575c5a30e875b9a4586fb77061c371
Risto McIntosh <Risto.mcintosh@gmail.com>
2020-06-23 21:21:33 -0500

    connect front end to back

---
+++ src/utils/api-client.ts
@@ -0,0 +1,31 @@
+const BASE_URL = 'http://localhost:5000/api';
+
+type ClientParams = {
+  data?: any;
+} & RequestInit;
+
+async function client<T>(
+  endpoint: string,
+  { data, ...customConfig }: ClientParams = {}
+): Promise<T> {
+  const config: RequestInit = {
+    method: data ? 'POST' : 'GET',
+    body: data ? JSON.stringify(data) : undefined,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    ...customConfig
+  };
+  console.log('passed data:', JSON.stringify(data));
+  return fetch(`${BASE_URL}${endpoint}`, config).then(async (response) => {
+    const data = await response.json();
+    console.log('api client:', data);
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(data);
+    }
+  });
+}
+
+export { client };
--- src/utils/kanban.ts
+++ src/utils/kanban.ts
@@ -1,4 +1,5 @@
 import * as kanbanClient from './kanban-client';
+import { client } from './api-client';
 import * as queryKey from './queryKeys';
 import { useQuery, useMutation, queryCache } from 'react-query';
 import { kanbanDataMap, kanbanColumn } from '../types';
@@ -6,47 +7,43 @@ import { kanbanDataMap, kanbanColumn } from '../types';
 function useKanbanData() {
   return useQuery({
     queryKey: queryKey.KANBAN_DATA,
-    queryFn: () => kanbanClient.read().then((data) => data)
+    queryFn: () => client<kanbanDataMap>('/kanban-board').then((data) => data)
   });
 }
 
-function updateKanbanColumn(column: kanbanColumn) {
-  const inCache = queryCache.getQueryData(
+function onColumnUpdate(column: kanbanColumn) {
+  queryCache.cancelQueries(queryKey.KANBAN_DATA);
+  const previousData = queryCache.getQueryData<kanbanDataMap>(
     queryKey.KANBAN_DATA
-  ) as kanbanDataMap;
+  );
 
-  return kanbanClient
-    .update({
-      ...inCache,
-      columns: { ...inCache.columns, [column.columnId]: column }
-    })
-    .then((data) => data);
+  if (previousData) {
+    queryCache.setQueryData<kanbanDataMap>(queryKey.KANBAN_DATA, {
+      ...previousData,
+      columns: {
+        ...previousData.columns,
+        [column.columnId]: column
+      }
+    });
+  }
+
+  return previousData;
 }
 
 function useKanbanColumnUpdate() {
-  return useMutation(updateKanbanColumn, {
-    onMutate: (column) => {
-      queryCache.cancelQueries(queryKey.KANBAN_DATA);
-      const previousData = queryCache.getQueryData<kanbanDataMap>(
-        queryKey.KANBAN_DATA
-      );
-
-      if (previousData) {
-        queryCache.setQueryData<kanbanDataMap>(queryKey.KANBAN_DATA, {
-          ...previousData,
-          columns: {
-            ...previousData.columns,
-            [column.columnId]: column
-          }
-        });
-      }
-
-      return previousData;
-    },
-    onError: (error, column, snapshotValue) =>
-      queryCache.setQueryData(queryKey.KANBAN_DATA, snapshotValue),
-    onSettled: () => queryCache.refetchQueries(queryKey.KANBAN_DATA)
-  });
+  return useMutation(
+    (column: kanbanColumn) =>
+      client<kanbanColumn>(`/kanban-board/column/${column.columnId}`, {
+        data: column,
+        method: 'PUT'
+      }).then((data) => data),
+    {
+      onMutate: onColumnUpdate,
+      onError: (error, column, snapshotValue) =>
+        queryCache.setQueryData(queryKey.KANBAN_DATA, snapshotValue),
+      onSettled: () => queryCache.refetchQueries(queryKey.KANBAN_DATA)
+    }
+  );
 }
 
 function useKanbanUpdate() {
